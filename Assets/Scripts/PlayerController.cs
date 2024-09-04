6e5f31707ad3cb0d9ce15f9a2cba3cceec0ad9d2
Avodden <152660887+Avodden@users.noreply.github.com>
2024-09-04 15:24:23 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool overworld; 
+    float xspeed;
+    float xdirection;
+    float xvector;
+    float yspeed;
+    float ydirection;
+    float yvector;
+
+   public bool overworld; 
 
     private void Start()
     {
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
+
+        xspeed = 5;
         
-        
+        yspeed = 5;
+
         if (overworld)
         {
             GetComponent<Rigidbody2D>().gravityScale = 0f;
         }
         else
         {
-            GetComponent<Rigidbody2D>().gravityScale = 1;
+            GetComponent<Rigidbody2D>().gravityScale = 1f;
         }
     }
 
     private void Update()
     {
-        
-    }
+        if (overworld)
+        {
+            yspeed = 5;
+        }
+        else
+        {
+            yspeed = 0;
+        }
+        xdirection = Input.GetAxis("Horizontal");
+        xvector = xspeed * xdirection * Time.deltaTime;
+        transform.Translate(xvector, 0, 0);
+
+        ydirection = Input.GetAxis("Vertical");
+        yvector = yspeed * ydirection * Time.deltaTime;
+        transform.Translate(0, yvector, 0);
+    } 
     
     //for organization, put other built-in Unity functions here
     
