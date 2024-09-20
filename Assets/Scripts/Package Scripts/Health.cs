30de0e6a09b3d06c1f1df84d7c16cc1dc0749d86
Avodden <152660887+Avodden@users.noreply.github.com>
2024-09-20 15:32:19 -0700

    health bar
    No lerp work

--- Assets/Scripts/Package Scripts/Health.cs
+++ Assets/Scripts/Package Scripts/Health.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
    GameManager gm;
-   
+
+   public Slider healthSlider;
+
+   public Slider easehealthSlider;
+
+   private float lerpSpeed = 0.05f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gm = FindObjectOfType<GameManager>();
-        gm.health_amount = 3;
+        gm.health_amount = 100;
+        
     }
 
     // Update is called once per frame
@@ -16,8 +24,21 @@ public class Health : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Spikes"))
         {
-            gm.health_amount -= 1;
+            gm.health_amount -= 10;
             print("you have " + gm.health_amount + " health left"); 
         }
     }
-}
+
+    void Update()
+    {
+        if (healthSlider.value != gm.health_amount)
+        {
+            healthSlider.value = gm.health_amount;
+        }
+
+        if (healthSlider.value != easehealthSlider.value)
+        {
+            easehealthSlider.value = MathF.SmoothStep(easehealthSlider.value, gm.health_amount, lerpSpeed);
+        }
+    }
+} 
