d42eda4246e0e0848c3fb6e48e598cb741180d59
Avodden <152660887+Avodden@users.noreply.github.com>
2024-12-09 15:28:21 -0800

    Jump code
    Absolutely done

--- Assets/Scripts/Package Scripts/PlayerController.cs
+++ Assets/Scripts/Package Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float xspeed;
+    float jumpspeed = 10f;
+    float xspeed = 6f;
     float xdirection;
     float xvector;
     float yspeed;
@@ -13,10 +14,18 @@ public class PlayerController : MonoBehaviour
     float yvector;
     float coin_amount;
 
-   public bool overworld; 
+   public bool overworld;
+   private bool can_jump;
+   private bool is_grounded;
+   private Rigidbody2D rb;
+
+   public Transform groundCheck;
+   public float groundRadius = 0.2f;
+   public LayerMask groundLayer;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
         coin_amount = 0;
@@ -27,16 +36,33 @@ public class PlayerController : MonoBehaviour
 
         if (overworld)
         {
-            GetComponent<Rigidbody2D>().gravityScale = 0f;
+            rb.gravityScale = 0f;
+            can_jump = false;
         }
         else
         {
-            GetComponent<Rigidbody2D>().gravityScale = 1f;
+            rb.gravityScale = 2f;
+            can_jump = true;
         }
     }
 
     private void Update()
     {
+        if (can_jump = true)
+        {
+            Debug.Log("Jump ready!");
+        }
+        
+        if (!overworld)
+        {
+            is_grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+            Debug.Log("Is Grounded: " + is_grounded);
+        }
+        
+        xdirection = Input.GetAxis("Horizontal");
+        xvector = xspeed * xdirection * Time.deltaTime;
+        transform.Translate(xvector, 0, 0);
+        
         if (overworld)
         {
             yspeed = 6;
@@ -45,13 +71,24 @@ public class PlayerController : MonoBehaviour
         {
             yspeed = 0;
         }
-        xdirection = Input.GetAxis("Horizontal");
-        xvector = xspeed * xdirection * Time.deltaTime;
-        transform.Translate(xvector, 0, 0);
-
+        
         ydirection = Input.GetAxis("Vertical");
         yvector = yspeed * ydirection * Time.deltaTime;
         transform.Translate(0, yvector, 0);
+
+        if (can_jump && is_grounded && Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump button pressed!");
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if (is_grounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpspeed);
+        }
     }
    
    
